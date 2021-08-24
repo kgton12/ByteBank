0287f77bc234aa11b6c02cbe9b33651a35744177
Welington Oliveira <56723945+kgton12@users.noreply.github.com>
2021-08-24 20:52:45 -0300

    commit

--- ByteBank/Cliente.cs
+++ ByteBank/Cliente.cs
@@ -1,19 +1,10 @@
 ﻿
 namespace ByteBank
 {
-<<<<<<< HEAD
   public class Cliente
   {
     public string Nome { get; set; }
     public string Cpf { get; set; }
     public string Profissao { get; set; }
   }
-=======
-    public class Cliente
-    {
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
-        public string Profissao { get; set; }
-    }
->>>>>>> a3d1302390df0f23180b9d09d958442597572cff
 }
