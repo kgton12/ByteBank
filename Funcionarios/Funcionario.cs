52e533282bd9e6f5294fc907da21a0142f83f83a
Wellington Oliveira <56723945+kgton12@users.noreply.github.com>
2021-08-24 20:48:51 -0300

    corrigindo commit

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ByteBank.Funcionarios
+{
+  public abstract class Funcionario
+  {
+    public static int TotalDeFuncionarios { get; private set; }
+    //0 - funcionario
+    //1- diretor
+    //2- designer
+    private int _tipo;
+    public string Nome { get; set; }
+    public string CPF { get; private set; }
+    public double Salario { get; protected set; }//protected permite que as classes derivadas tambem consigam usar o set.
+
+    public Funcionario(double salario, string cpf)
+    {
+      CPF = cpf;
+      Salario = salario;
+      TotalDeFuncionarios++;
+    }
+
+    public abstract double GetBonificacao();
+
+    public abstract void AumentarSalario();
+
+  }
+}
